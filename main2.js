c7dd050f78c8810fb91db48d5b3abaf64f2453d5
dan5-gmail <kepa.kachi310@gmail.com>
2026-02-28 10:45:12 +0900

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+console.log(THREE);
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { PointerLockControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/PointerLockControls.js';
 
